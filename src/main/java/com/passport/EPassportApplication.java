36274ef5181f25d60d829202b762b4b8fcd3a565
sumanCap <122872791+sumanCap@users.noreply.github.com>
2023-03-13 22:27:26 +0530

    first commit

---
+++ src/main/java/com/passport/EPassportApplication.java
@@ -0,0 +1,13 @@
+package com.passport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EPassportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EPassportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/passport/EPassportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EPassportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
