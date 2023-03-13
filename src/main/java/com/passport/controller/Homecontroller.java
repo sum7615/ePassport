6cdd1357de51ae705a739a282da99a8235dc008d
sum7615 <sum793763@gmail.com>
2023-03-13 23:37:38 +0530

    Adding home page html

---
+++ src/main/java/com/passport/controller/Homecontroller.java
@@ -0,0 +1,13 @@
+package com.passport.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Homecontroller {
+
+	@GetMapping("/")
+	public String home() {
+		return "home.html";
+	}
+}
